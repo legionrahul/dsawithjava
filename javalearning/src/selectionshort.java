2f310856d67044bfff2d3d191dae42cc3ab9a208
rahul <legionrahull@gmail.com>
2024-06-11 10:34:36 +0530

    learnning java

---
+++ javalearning/src/selectionshort.java
@@ -0,0 +1,38 @@
+public class selectionshort {
+    public static void main(String[]args)
+    {
+        int arr[]=new int[]{5,4,6,9,1};
+        int smallest =arr[0];
+        
+        
+        for(int i=0;i<arr.length-1;i++)
+        for(int j=i+1;j<arr.length;j++)
+        {
+        if(arr[i]<=smallest)
+    {
+         smallest=j;
+         
+    }
+    int temp=smallest;
+    smallest=arr[i];
+    arr[i]=temp;
+    for (int num : arr) {
+        System.out.print(num + " ");
+    }
+     
+        
+
+
+        
+      
+        }
+        
+         
+         
+
+
+        
+
+    }
+    
+}
